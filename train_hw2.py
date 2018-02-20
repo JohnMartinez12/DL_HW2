2bea54658cce7f5c6251a64fd785f5970922165e
vvb231 <31828237+vvb231@users.noreply.github.com>
2018-02-20 16:24:56 -0500

    Add files via upload

---
+++ train_hw2.py
@@ -0,0 +1,321 @@
+import tensorflow as tf
+import time
+from datetime import timedelta
+import math
+import numpy as np
+import os
+import glob
+import sys
+import scipy.misc
+from scipy.ndimage import imread
+
+
+batch_size = 32
+
+num_classes = 10
+
+
+img_size = 32
+num_channels = 3
+
+# ================== HELPER FUNCTIONS ================== 
+
+def get_img_array(path):
+    """
+    Given path of image, returns it's numpy array
+    """
+    return scipy.misc.imread(path)
+
+def get_files(folder):
+    """
+    Given path to folder, returns list of files in it
+    """
+    filenames = [file for file in glob.glob(folder+'*/*')]
+    filenames.sort()
+    return filenames
+
+def get_label(filepath, label2id):
+    """
+    Files are assumed to be labeled as: /path/to/file/999_frog.png
+    Returns label for a filepath
+    """
+    tokens = filepath.split('/')
+    label = tokens[-1].split('_')[1][:-4]
+    if label in label2id:
+        return label2id[label]
+    else:
+        sys.exit("Invalid label: " + label)
+        
+
+def get_labels(folder, label2id):
+    """
+    Returns vector of labels extracted from filenames of all files in folder
+    :param folder: path to data folder
+    :param label2id: mapping of text labels to numeric ids. (Eg: automobile -> 0)
+    """
+    files = get_files(folder)
+    y = []
+    for f in files:
+        y.append(get_label(f,label2id))
+    return np.array(y)
+
+def one_hot(y, num_classes=10):
+    """
+    Converts each label index in y to vector with one_hot encoding
+    One-hot encoding converts categorical labels to binary values
+    """
+    y_one_hot = np.zeros((num_classes, y.shape[0]))
+    y_one_hot[y, range(y.shape[0])] = 1
+    return y_one_hot
+
+def get_label_mapping(label_file):
+    """
+    Returns mappings of label to index and index to label
+    The input file has list of labels, each on a separate line.
+    """
+    print(os.listdir())
+    with open(label_file, 'r') as f:
+        id2label = f.readlines()
+        id2label = [l.strip() for l in id2label]
+    label2id = {}
+    count = 0
+    for label in id2label:
+        label2id[label] = count
+        count += 1
+    return id2label, label2id
+
+def get_images(folder):
+    """
+    returns numpy array of all samples in folder
+    each column is a sample resized to 30x30 and flattened
+    """
+    files = get_files(folder)
+    images = []
+    count = 0
+    
+    for f in files:
+        count += 1 
+        if count % 10000 == 0:
+            print("Loaded {}/{}".format(count,len(files)))
+        img_arr = get_img_array(f)
+        img_arr = img_arr.flatten() / 255.0
+        images.append(img_arr)
+    X = np.column_stack(images)
+
+    return X
+
+def get_train_data(data_root_path):
+    """
+    Return X and y
+    """
+    train_data_path = data_root_path + 'train'
+    id2label, label2id = get_label_mapping(data_root_path+'labels.txt')
+    print(label2id)
+    X = get_images(train_data_path)
+    y = get_labels(train_data_path, label2id)
+    return X, y
+
+def save_predictions(filename, y):
+    """
+    Dumps y into .npy file
+    """
+    np.save(filename, y)
+    
+def get_batch(X, y, batch_size):
+    """
+    Return minibatch of samples and labels
+  
+    :param X, y: samples and corresponding labels
+    :parma batch_size: minibatch size
+    :returns: (tuple) X_batch, y_batch
+    """
+    # Random indices for the samples
+
+    indices = np.random.randint(y.shape[0]-1, size= batch_size)
+
+    X_batch = X[indices, :]
+    y_batch = y[indices, :]
+  
+    return X_batch, y_batch
+
+# Load the data
+data_root_path = 'HW2_data/'
+X_train, y_train = get_train_data(data_root_path) # this may take a few minutes
+y_train = one_hot(y_train).T
+X_train = X_train.T
+X_train = (X_train - np.mean(X_train,axis=0)) / np.std(X_train,axis=0)
+print("Data loading done.")
+
+
+
+session = tf.Session()
+x = tf.placeholder(tf.float32, shape=[None, img_size,img_size,num_channels], name='x')
+
+## labels
+y_true = tf.placeholder(tf.float32, shape=[None, num_classes], name='y_true')
+y_true_cls = tf.argmax(y_true, dimension=1)
+
+
+
+##Network graph params
+filter_size_conv1 = 3 
+num_filters_conv1 = 32
+
+filter_size_conv2 = 3
+num_filters_conv2 = 32
+
+filter_size_conv3 = 3
+num_filters_conv3 = 64
+    
+fc_layer_size = 128
+
+def create_weights(shape):
+    return tf.Variable(tf.truncated_normal(shape, stddev=0.05))
+
+def create_biases(size):
+    return tf.Variable(tf.constant(0.05, shape=[size]))
+
+
+
+def create_convolutional_layer(input,
+               num_input_channels, 
+               conv_filter_size,        
+               num_filters):  
+    
+    ## We shall define the weights that will be trained using create_weights function.
+    weights = create_weights(shape=[conv_filter_size, conv_filter_size, num_input_channels, num_filters])
+    ## We create biases using the create_biases function. These are also trained.
+    biases = create_biases(num_filters)
+
+    ## Creating the convolutional layer
+    layer = tf.nn.conv2d(input=input,
+                     filter=weights,
+                     strides=[1, 1, 1, 1],
+                     padding='SAME')
+
+    layer += biases
+
+    ## We shall be using max-pooling.  
+    layer = tf.nn.max_pool(value=layer,
+                            ksize=[1, 2, 2, 1],
+                            strides=[1, 2, 2, 1],
+                            padding='SAME')
+    ## Output of pooling is fed to Relu which is the activation function for us.
+    layer = tf.nn.relu(layer)
+
+    return layer
+
+    
+
+def create_flatten_layer(layer):
+    #We know that the shape of the layer will be [batch_size img_size img_size num_channels] 
+    # But let's get it from the previous layer.
+    layer_shape = layer.get_shape()
+
+    ## Number of features will be img_height * img_width* num_channels. But we shall calculate it in place of hard-coding it.
+    num_features = layer_shape[1:4].num_elements()
+
+    ## Now, we Flatten the layer so we shall have to reshape to num_features
+    layer = tf.reshape(layer, [-1, num_features])
+
+    return layer
+
+
+def create_fc_layer(input,          
+             num_inputs,    
+             num_outputs,
+             use_relu=True):
+    
+    #Let's define trainable weights and biases.
+    weights = create_weights(shape=[num_inputs, num_outputs])
+    biases = create_biases(num_outputs)
+
+    # Fully connected layer takes input x and produces wx+b.Since, these are matrices, we use matmul function in Tensorflow
+    layer = tf.matmul(input, weights) + biases
+    if use_relu:
+        layer = tf.nn.relu(layer)
+
+    return layer
+
+
+layer_conv1 = create_convolutional_layer(input=x,
+               num_input_channels=num_channels,
+               conv_filter_size=filter_size_conv1,
+               num_filters=num_filters_conv1)
+layer_conv2 = create_convolutional_layer(input=layer_conv1,
+               num_input_channels=num_filters_conv1,
+               conv_filter_size=filter_size_conv2,
+               num_filters=num_filters_conv2)
+
+layer_conv3= create_convolutional_layer(input=layer_conv2,
+               num_input_channels=num_filters_conv2,
+               conv_filter_size=filter_size_conv3,
+               num_filters=num_filters_conv3)
+          
+layer_flat = create_flatten_layer(layer_conv3)
+
+layer_fc1 = create_fc_layer(input=layer_flat,
+                     num_inputs=layer_flat.get_shape()[1:4].num_elements(),
+                     num_outputs=fc_layer_size,
+                     use_relu=True)
+
+layer_fc2 = create_fc_layer(input=layer_fc1,
+                     num_inputs=fc_layer_size,
+                     num_outputs=num_classes,
+                     use_relu=False) 
+
+y_pred = tf.nn.softmax(layer_fc2,name='y_pred')
+
+y_pred_cls = tf.argmax(y_pred, dimension=1)
+session.run(tf.global_variables_initializer())
+cross_entropy = tf.nn.softmax_cross_entropy_with_logits(logits=layer_fc2,
+                                                    labels=y_true)
+cost = tf.reduce_mean(cross_entropy)
+optimizer = tf.train.AdamOptimizer(learning_rate=1e-3).minimize(cost)
+correct_prediction = tf.equal(y_pred_cls, y_true_cls)
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+
+session.run(tf.global_variables_initializer()) 
+
+
+def show_progress(epoch, feed_dict_train, feed_dict_validate, val_loss):
+    acc = session.run(accuracy, feed_dict=feed_dict_train)
+    val_acc = session.run(accuracy, feed_dict=feed_dict_validate)
+    msg = "Training Epoch {0} --- Training Accuracy: {1:>6.1%}, Validation Accuracy: {2:>6.1%},  Validation Loss: {3:.3f}"
+    print(msg.format(epoch + 1, acc, val_acc, val_loss))
+
+total_iterations = 0
+
+saver = tf.train.Saver()
+def train(num_iteration):
+    global total_iterations
+    
+    for i in range(total_iterations,
+                   total_iterations + num_iteration):
+        
+        x_batch, y_true_batch = get_batch(X_train, y_train, batch_size)
+        x_batch = x_batch.reshape((batch_size, 32, 32, 3))
+        
+        x_valid_batch, y_valid_batch = get_batch(X_train, y_train, batch_size)
+        x_valid_batch = x_valid_batch.reshape((batch_size, 32, 32, 3))
+        
+        
+        feed_dict_tr = {x: x_batch,
+                           y_true: y_true_batch}
+        feed_dict_val = {x: x_valid_batch,
+                              y_true: y_valid_batch}
+
+        session.run(optimizer, feed_dict=feed_dict_tr)
+
+        if i % int(45000/batch_size) == 0: 
+            val_loss = session.run(cost, feed_dict=feed_dict_tr)
+            
+            epoch = int(i / int(45000/batch_size))    
+            
+            show_progress(epoch, feed_dict_tr, feed_dict_val, val_loss)
+
+
+    total_iterations += num_iteration
+
+train(num_iteration=7000)
